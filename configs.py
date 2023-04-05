cf2944a12a4c46e0a4e59ad96bd132bb39781ec2
Muhammedshifin <muhammedshifink4@gmail.com>
2023-04-05 12:53:52 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,12 +1,12 @@
 from os import path, getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", "0112234"))
-    API_HASH = getenv("API_HASH", "abcdefg")
-    BOT_TOKEN = getenv("BOT_TOKEN", "1234567891:AdDfgFRFVVfDEhdhyjjvjjftSEW")
-    FSUB = getenv("FSUB", "SDBotz")
-    CHID = int(getenv("CHID", "-1000112234"))
+    API_ID = int(getenv("API_ID", "18302370"))
+    API_HASH = getenv("API_HASH", "03c2cced4dea9b1e96dce87558dd2381")
+    BOT_TOKEN = getenv("BOT_TOKEN", "5924932917:AAFHC6sQkot3C1GHeurOuY_K3eG-75KJ7LY")
+    FSUB = getenv("FSUB", "CinemavillaAutoAccept")
+    CHID = int(getenv("CHID", "-1001234100577"))
     SUDO = list(map(int, getenv("SUDO").split()))
-    MONGO_URI = getenv("MONGO_URI", "")
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://Mst:Mst@cluster0.0sjrq6j.mongodb.net/?retryWrites=true&w=majority")
     
 cfg = Config()
